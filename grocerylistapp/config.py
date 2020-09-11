2f12b1cd0145a14e55efd7a5a0ca96adbc5caf4d
Steve Wenzel <wenzelstev@gmail.com>
2020-09-11 12:42:42 +0000

    committing deployed version

--- grocerylistapp/config.py
+++ grocerylistapp/config.py
@@ -1,13 +1,15 @@
-import os
+from decouple import config
 
+import os
+from sqlalchemy import create_engine
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("EMAIL_USER")
-    MAIL_PASSWORD = os.environ.get("EMAIL_PASS")
+    MAIL_USERNAME = config('EMAIL_USER')
+    MAIL_PASSWORD = config('EMAIL_PASS')
     #SQLALCHEMY_ECHO = True
